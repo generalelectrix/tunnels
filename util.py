8e4c7cc5e98c8aa647fc7cd6f5b2e2495f1e3ebe
generalelectrix <general.electrix@gmail.com>
2016-02-29 21:36:48 -0800

    ENH: port beam_vault

--- util.py
+++ util.py
@@ -1,8 +1,3 @@
-
-def copy_list_of_beams(to_copy):
-    """Deep copy a list of beams."""
-    return [beam.copy() for beam in to_copy]
-
 def unwrap(angle):
     """Unwrap an angle in radians."""
     while PI < angle:
