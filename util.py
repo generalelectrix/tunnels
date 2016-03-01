d6eac6e20fc58cb50afb8121225201439736e5ac
generalelectrix <general.electrix@gmail.com>
2016-02-29 21:25:53 -0800

    LNT: indentation

--- util.py
+++ util.py
@@ -1,14 +1,14 @@
 
 def copy_list_of_beams(to_copy):
-  """Deep copy a list of beams."""
-  return [beam.copy() for beam in to_copy]
+    """Deep copy a list of beams."""
+    return [beam.copy() for beam in to_copy]
 
 def unwrap(angle):
-  """Unwrap an angle in radians."""
-  while PI < angle:
-    angle = angle - TWO_PI
+    """Unwrap an angle in radians."""
+    while PI < angle:
+        angle = angle - TWO_PI
 
-  while PI < -1*angle:
-    angle = angle + TWO_PI
+    while PI < -1*angle:
+        angle = angle + TWO_PI
 
-  return angle
+    return angle
