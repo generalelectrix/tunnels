2c094338df9b52babf35b75a85627c46de0eadf3
generalelectrix <general.electrix@gmail.com>
2016-03-17 23:59:32 -0700

    ENH: move waveforms into cython

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+from Cython.Build import cythonize
 try:
     from setuptools import setup, Extension
 except ImportError:
     from distutils.core import setup, Extension
+import numpy
 
-requires = ['numpy', 'scipy', 'protobuf', 'bidict']
+requires = ['numpy', 'scipy', 'bidict', 'cython']
+
+extensions = [
+    Extension(
+        "tunnelz/waveforms",
+        ["tunnelz/waveforms.pyx"],
+        include_dirs=[numpy.get_include()],)]
 
 setup(
     name='tunnelz',
     install_requires=requires,
     license='GPL2',
-    ext_modules=[Extension(
-        'draw_commands',
-        sources=['cpp/draw_commands.c', 'cpp/draw_commands.pb.cc'],
-        libraries=['protobuf'])],
+    ext_modules=cythonize(extensions),
 )
--- tunnelz/waveforms.pyx
+++ tunnelz/waveforms.pyx
@@ -1,12 +1,14 @@
+from __future__ import division
 from math import pi
-from numpy import vectorize
+import numpy as np
+cimport numpy as np
 from scipy import signal
 
-PI = pi
-HALF_PI = pi / 2.0
-TWOPI = 2*pi
+cdef float PI = np.pi
+cdef float HALF_PI = np.pi / 2.0
+cdef float TWOPI = 2*np.pi
 
-def triangle(angle):
+cdef inline triangle (float angle):
     """Generate a point on a unit triangle wave from angle in radians."""
     angle = (angle % TWOPI) / TWOPI
 
@@ -17,7 +19,7 @@ def triangle(angle):
     else:
         return 2.0 - 4.0 * angle
 
-def square(angle, smoothing):
+cdef inline float square(float angle, float smoothing):
     """Generate a point on a square wave from angle in radians and smoothing."""
 
     angle = (angle % TWOPI)
@@ -39,7 +41,7 @@ def square(angle, smoothing):
     else:
         return -1.0
 
-def sawtooth(angle, smoothing):
+cdef inline float sawtooth(float angle, float smoothing):
     """Generate a point on a sawtooth wave from angle in radians and smoothing."""
 
     angle = (angle % TWOPI)
@@ -57,12 +59,33 @@ def sawtooth(angle, smoothing):
     else:
         return -(angle - PI)/smoothing
 
-def sawtooth_vector(angles, smoothing):
+def sawtooth_vector(np.ndarray[np.float_t, ndim=1] angles, np.float smoothing):
     """Generate wavetooth wave points from angle in radians and smoothing."""
-    width = 1.0 - smoothing / PI
-    return signal.sawtooth(angles + width/2.0, width)
+    cdef int size = angles.shape[0]
+    cdef np.ndarray[np.float_t, ndim=1] output = np.empty(size, np.float)
+    cdef size_t i
 
-def triangle_vector(angles):
-    return signal.sawtooth(angles, 0.5)
+    for i in xrange(size):
+        output[i] = sawtooth(angles[i], smoothing)
 
-square_vector = vectorize(square)
+    return output
+
+def triangle_vector(np.ndarray[np.float_t, ndim=1] angles, np.float smoothing):
+    cdef int size = angles.shape[0]
+    cdef np.ndarray[np.float_t, ndim=1] output = np.empty(size, np.float)
+    cdef size_t i
+
+    for i in xrange(size):
+        output[i] = triangle(angles[i])
+
+    return output
+
+def square_vector(np.ndarray[np.float_t, ndim=1] angles, np.float smoothing):
+    cdef int size = angles.shape[0]
+    cdef np.ndarray[np.float_t, ndim=1] output = np.empty(size, np.float)
+    cdef size_t i
+
+    for i in xrange(size):
+        output[i] = square(angles[i], smoothing)
+
+    return output
