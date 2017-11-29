0cd64d7b0315e336e07bae67460f8d2b81eddc07
Chris Macklin <chris.macklin@gmail.com>
2017-11-29 02:11:52 -0800

    improve error messages on config parsing

--- tunnelclient/src/config.rs
+++ tunnelclient/src/config.rs
@@ -25,7 +25,6 @@ pub struct ClientConfig {
 /// Loads, parses, and returns the config.
 /// Panics if something goes wrong.
 pub fn config_from_command_line() -> ClientConfig {
-
     let video_channel_str = env::args().nth(1).expect("No video channel provided.");
 
     // Parse video channel as an int and blow up if it isn't one.
@@ -39,25 +38,26 @@ pub fn config_from_command_line() -> ClientConfig {
     config_file.read_to_string(&mut config_file_string).unwrap();
     let docs = YamlLoader::load_from_str(&config_file_string).unwrap();
     let cfg = &docs[0];
-    let x_resolution = cfg["x_resolution"].as_i64().unwrap() as u32;
-    let y_resolution = cfg["y_resolution"].as_i64().unwrap() as u32;
+    let x_resolution = cfg["x_resolution"].as_i64().expect("Bad x resolution.") as u32;
+    let y_resolution = cfg["y_resolution"].as_i64().expect("Bad y resolution.") as u32;
     let host = cfg["server_hostname"].as_str().unwrap().trim().to_string();
-    let timesync_interval = Duration::from_millis(cfg["timesync_interval"].as_i64().unwrap() as u64);
+    let timesync_interval = Duration::from_millis(
+        cfg["timesync_interval"].as_i64().expect("Bad timesync_interval.") as u64);
 
     println!("Running on video channel {}.", channel_filter_str);
     ClientConfig {
         server_hostname: host,
         video_channel: channel_filter_str,
-        render_delay: cfg["render_delay"].as_f64().unwrap(),
+        render_delay: cfg["render_delay"].as_i64().expect("Bad render delay.") as f64,
         timesync_interval,
         x_resolution,
         y_resolution,
-        anti_alias: cfg["anti_alias"].as_bool().unwrap(),
-        fullscreen: cfg["fullscreen"].as_bool().unwrap(),
+        anti_alias: cfg["anti_alias"].as_bool().expect("Bad anti-alias flag."),
+        fullscreen: cfg["fullscreen"].as_bool().expect("Bad fullscreen flag."),
         critical_size: cmp::min(x_resolution, y_resolution) as f64,
         thickness_scale: 0.5,
         x_center: (x_resolution / 2) as f64,
         y_center: (y_resolution / 2) as f64,
-        alpha_blend: cfg["alpha_blend"].as_bool().unwrap()
+        alpha_blend: cfg["alpha_blend"].as_bool().expect("Bad alpha_blend flag.")
     }
 }
