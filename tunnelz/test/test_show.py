dd6342090d6c1ab1ad84559218f8a918becc5b79
Chris Macklin <chris.macklin@gmail.com>
2017-12-11 22:41:47 -0800

    add a simple checksum to layers to catch regressions

--- tunnelz/test/test_show.py
+++ tunnelz/test/test_show.py
@@ -4,6 +4,9 @@ from nose.tools import assert_equal
 from tunnelz import Show, DEFAULT_CONFIG
 from tunnelz.show import N_VIDEO_CHANNELS
 
+def layer_checksum(layer):
+    return sum(val for draw_call in layer for val in draw_call)
+
 class TestShow (object):
 
     test_save_file_path = "tunnel_test_save.test"
@@ -31,6 +34,29 @@ class TestShow (object):
         # each beam should have some draw calls
         for layer in ch0:
             assert layer
+
+        # checksum on the layers to catch generic unexpected changes
+        # this may turn out to be platform-dependent as it is quite crude
+        layer_checksums = [
+            625.78766366950549,
+            626.6027261695067,
+            625.30911366950704,
+            625.90682616950699,
+            626.39586366950641,
+            626.77622616950839,
+            627.04791366950667,
+            627.21092616950716,
+            625.26526366950759,
+            625.31960116950847,
+            625.48261366950692,
+            625.75430116950668,
+            626.13466366950649,
+            626.6237011695091,
+            627.22141366950882,
+            625.92780116950814]
+
+        for i, layer in enumerate(ch0):
+            assert_equal(layer_checksums[i], layer_checksum(layer))
 
     def tearDown(self):
         try:
