99bb58f59c53c02fa63fd83cd4ebc5e41fc60cf5
Chris Macklin <chris.macklin@gmail.com>
2017-12-26 15:30:43 -0800

    implement tap sync and a quick test script

--- tunnelz/clock.py
+++ tunnelz/clock.py
@@ -1,4 +1,6 @@
 import copy
+from itertools import izip, tee
+from monotonic import monotonic
 
 class Clock (object):
 
@@ -12,3 +14,61 @@ class Clock (object):
 
     def copy(self):
         return copy.copy(self)
+
+
+class TapSync (object):
+    """Handle estimation of phase and rate from a series of taps."""
+    # fractional threshold at which we'll discard the current tap buffer and
+    # start a new one
+    reset_threshold = 0.1
+
+    def __init__(self):
+        self._tap_times = []
+        self._rate = None
+        self._period = None
+
+    @property
+    def rate(self):
+        return self._rate
+
+    def _reset_buffer(self, tap_time):
+        print "reset"
+        self._tap_times = [tap_time]
+        self._rate = None
+        self._period = None
+
+    def _add_tap(self, tap_time):
+        """Add a tap time to the buffer and update our value estimates if possible."""
+        self._tap_times.append(tap_time)
+        if len(self._tap_times) > 1:
+            # iterate over all pairs of times to get differences
+            first_iter, second_iter = tee(self._tap_times)
+            next(second_iter, None)
+            deltas = list(second - first for first, second in izip(first_iter, second_iter))
+
+            self._period = sum(deltas) / len(deltas)
+            self._rate = 1.0 / self._period
+            print "new estimate: ", self._rate
+
+    def tap(self):
+        """Register a new tap event, and compute a new estimated bpm."""
+        now = monotonic()
+        print "tap"
+
+        # if the tap buffer isn't empty, determine elapsed time from the last
+        # tap to this one
+        if self._period is not None:
+            dt = now - self._tap_times[-1]
+
+            # if this single estimate of tempo is within +-10% of current, use it
+            # otherwise, empty the buffer and start over
+            fractional_difference = (self._period - dt) / self._period
+
+            if abs(fractional_difference) > 0.1:
+                # outlier, empty the buffer
+                self._reset_buffer(now)
+            else:
+                # append to buffer and update
+                self._add_tap(now)
+        else:
+            self._add_tap(now)
