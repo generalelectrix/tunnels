4100ecc287c4a980122954b25b12b4090e461bb1
Chris Macklin <chris.macklin@gmail.com>
2018-01-19 21:29:02 -0800

    successfully resolve

--- zeroconf_client/src/main.rs
+++ zeroconf_client/src/main.rs
@@ -2,19 +2,32 @@ extern crate async_dnssd;
 extern crate tokio_core;
 extern crate futures;
 
-use async_dnssd::{browse, Interface};
+use async_dnssd::{browse, BrowsedFlag, Interface};
 use futures::Stream;
 use tokio_core::reactor::Core;
 
 fn main() {
     let mut core = Core::new().unwrap();
 
+    let handle = core.handle();
+
     let browse_result = browse(
                                 Interface::Any,
                                 "_tunnel._tcp",
                                 None,
-                                &core.handle())
+                                &handle)
         .unwrap()
+        .filter_map(|item| {
+            println!("{:?}", item);
+            // check if this service was added
+            if item.flags & BrowsedFlag::Add {
+                Some(item)
+            } else {
+                None
+            }
+        })
+        .and_then(|item| item.resolve(&handle))
+        .flatten()
         .for_each(|item| {
             println!("{:?}", item);
             Ok(())
