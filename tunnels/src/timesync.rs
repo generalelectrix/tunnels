f658dade3bb470fd7b5d622b6f87ffe304a98df3
general electrix <general.electrix@gmail.com>
2021-04-15 01:10:46 -0700

    Implement timesync server.

--- tunnels/src/timesync.rs
+++ tunnels/src/timesync.rs
@@ -1,18 +1,52 @@
-use std::time::Instant;
+use log::error;
+use std::thread;
+use std::{error::Error, time::Duration, time::Instant};
 
+use rmp_serde::Serializer;
+use serde::Serialize;
+use tunnels_lib::RunFlag;
 use zmq;
-use zmq::{Context, Socket};
+use zmq::Context;
 
-pub struct Server {
-    socket: Socket,
+pub fn run(
+    ctx: &mut Context,
+    port: u16,
     start: Instant,
-}
+    run: RunFlag,
+) -> Result<thread::JoinHandle<()>, Box<dyn Error>> {
+    let socket = ctx.socket(zmq::REP)?;
+    let addr = format!("tcp://*:{}", port);
+    socket.bind(&addr)?;
+    // time out once per second
+    socket.set_rcvtimeo(1000)?;
 
-impl Server {
-    pub fn new(ctx: &mut Context, port: u16, start: Instant) -> Result<Self, Box<dyn Error>> {
-        let socket = ctx.socket(zmq::REP)?;
-        let addr = format!("tcp://*:{}", port);
-        socket.bind(addr)?;
-        Ok(Self { socket, start })
-    }
+    // start up the service in a new thread
+    let jh = thread::Builder::new()
+        .name("timesync".to_string())
+        .spawn(move || {
+            let mut resp_buf = Vec::new();
+            loop {
+                if !run.should_run() {
+                    return;
+                }
+
+                match socket.recv_bytes(0) {
+                    Err(zmq::Error::EAGAIN) => (),
+                    Err(e) => {
+                        error!("Timesync receieve error: {}.", e);
+                    }
+                    Ok(_) => {
+                        let now = start.elapsed().as_secs_f64();
+                        if let Err(e) = now.serialize(&mut Serializer::new(&mut resp_buf)) {
+                            error!("Timesync serialization error: {}.", e);
+                        }
+                        if let Err(e) = socket.send(&resp_buf, 0) {
+                            error!("Timesync send error: {}.", e);
+                        }
+                    }
+                }
+                resp_buf.clear();
+            }
+        })?;
+    Ok(jh)
 }
