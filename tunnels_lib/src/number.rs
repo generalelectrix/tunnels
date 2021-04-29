c2b11616bd56be7051bbe65b5cfa39cbea7008c1
general electrix <general.electrix@gmail.com>
2021-04-28 23:14:46 -0700

    Update docstring on number mod.

--- tunnels_lib/src/number.rs
+++ tunnels_lib/src/number.rs
@@ -1,1 +1,5 @@
+//! Re-export the number crate under local scope.
+//! TODO: in the future it might make sense to remove this re-export and refer
+//! to the number crate directly.
+
 pub use number::*;
