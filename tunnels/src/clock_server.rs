4ee0dd17fbdfe3cb8759bfc7bed7422457d94543
general electrix <general.electrix@gmail.com>
2023-03-26 01:37:45 -0700

    Implement static clock data holders.

--- tunnels/src/clock_server.rs
+++ tunnels/src/clock_server.rs
@@ -2,8 +2,39 @@
 //! Provide a strongly-typed receiver.
 //! FIXME: would be nice to clean up deserialization to avoid so many allocations.
 
+use tunnels_lib::number::{Phase, UnipolarFloat};
 use zero_configure::pub_sub::PublisherService;
 
-use crate::clock_bank::ClockBank;
+use crate::clock_bank::{ClockBank, ClockIdx, ClockStore, N_CLOCKS};
 
 pub type ClockPublisher = PublisherService<ClockBank>;
+
+/// A collection of static clock state data, rendered from a ClockBank.
+pub struct StaticClockBank([StaticClock; N_CLOCKS]);
+
+impl ClockStore for StaticClockBank {
+    fn phase(&self, index: ClockIdx) -> Phase {
+        self.get(index).phase
+    }
+
+    fn submaster_level(&self, index: ClockIdx) -> UnipolarFloat {
+        self.get(index).submaster_level
+    }
+
+    fn use_audio_size(&self, index: ClockIdx) -> bool {
+        self.get(index).use_audio_size
+    }
+}
+
+impl StaticClockBank {
+    fn get(&self, index: ClockIdx) -> &StaticClock {
+        &self.0[usize::from(index)]
+    }
+}
+
+/// A static snapshot of clock state.
+pub struct StaticClock {
+    phase: Phase,
+    submaster_level: UnipolarFloat,
+    use_audio_size: bool,
+}
